3dbe31d582819f5da019ce93a706700f8d4e53e3
qvarkk <xumdes@gmail.com>
2025-02-05 17:53:18 +0500

    Added DOM Manager for State Machine

--- lab-1/index.js
+++ lab-1/index.js
@@ -13,48 +13,140 @@ var IncomingSignal;
     IncomingSignal[IncomingSignal["GREEN"] = 2] = "GREEN";
     IncomingSignal[IncomingSignal["YELLOW"] = 3] = "YELLOW";
 })(IncomingSignal || (IncomingSignal = {}));
-class TrafficLight {
+class TrafficLightStateMachine {
     get state() {
         return this._state;
     }
     constructor() {
         this._state = State.OFF;
     }
-    send_signal(signal) {
+    sendSignal(signal) {
         switch (signal) {
             case IncomingSignal.RED:
-                this.handle_red_signal();
+                this.handleRedSignal();
                 break;
             case IncomingSignal.YELLOW_RED:
-                this.handle_yellow_red_signal();
+                this.handleYellowRedSignal();
                 break;
             case IncomingSignal.GREEN:
-                this.handle_green_signal();
+                this.handleGreenSignal();
                 break;
             case IncomingSignal.YELLOW:
-                this.handle_yellow_signal();
+                this.handleYellowSignal();
                 break;
         }
         return this.state;
     }
-    handle_red_signal() {
+    handleRedSignal() {
         if (this._state == State.OFF || this._state == State.YELLOW) {
             this._state = State.RED;
         }
     }
-    handle_yellow_red_signal() {
+    handleYellowRedSignal() {
         if (this._state == State.RED) {
             this._state = State.YELLOW_RED;
         }
     }
-    handle_green_signal() {
+    handleGreenSignal() {
         if (this._state == State.YELLOW_RED) {
             this._state = State.GREEN;
         }
     }
-    handle_yellow_signal() {
+    handleYellowSignal() {
         if (this._state == State.GREEN) {
             this._state = State.YELLOW;
         }
     }
 }
+class TrafficLightDomManager {
+    constructor() {
+        this.redCarLight = document.querySelector('#redCarLight');
+        this.yellowCarLight = document.querySelector('#yellowCarLight');
+        this.greenCarLight = document.querySelector('#greenCarLight');
+        this.redPedestrianLight = document.querySelector('#redPedestrianLight');
+        this.greenPedestrianLight = document.querySelector('#greenPedestrianLight');
+        if (!this.redCarLight || !this.yellowCarLight || !this.greenCarLight ||
+            !this.redPedestrianLight || !this.greenPedestrianLight) {
+            alert('Not all required elements are present on page!');
+        }
+        this.stateMachine = new TrafficLightStateMachine();
+        this.handleState();
+        this._time = 0;
+    }
+    work() {
+        setInterval(() => {
+            this.handleTimer();
+            this._time++;
+        }, 1000);
+    }
+    handleTimer() {
+        if (this._time == 0) {
+            this.sendSignal(IncomingSignal.RED);
+        }
+        else if (this._time == 10) {
+            this.sendSignal(IncomingSignal.YELLOW_RED);
+        }
+        else if (this._time == 12) {
+            this.sendSignal(IncomingSignal.GREEN);
+        }
+        else if (this._time == 22) {
+            this.sendSignal(IncomingSignal.YELLOW);
+        }
+        else if (this._time == 24) {
+            this.sendSignal(IncomingSignal.RED);
+            this._time = 0;
+        }
+    }
+    sendSignal(signal) {
+        this.stateMachine.sendSignal(signal);
+        this.handleState();
+    }
+    handleState() {
+        switch (this.stateMachine.state) {
+            case State.OFF:
+                this.handleOffState();
+                break;
+            case State.RED:
+                this.handleRedState();
+                break;
+            case State.YELLOW_RED:
+                this.handleYellowRedState();
+                break;
+            case State.GREEN:
+                this.handleGreenState();
+                break;
+            case State.YELLOW:
+                this.handleYellowState();
+                break;
+        }
+    }
+    handleOffState() {
+        this.redCarLight.classList.add('inactive');
+        this.yellowCarLight.classList.add('inactive');
+        this.greenCarLight.classList.add('inactive');
+        this.redPedestrianLight.classList.add('inactive');
+        this.greenPedestrianLight.classList.add('inactive');
+    }
+    handleRedState() {
+        this.redCarLight.classList.remove('inactive');
+        this.yellowCarLight.classList.add('inactive');
+        this.redPedestrianLight.classList.add('inactive');
+        this.greenPedestrianLight.classList.remove('inactive');
+    }
+    handleYellowRedState() {
+        this.yellowCarLight.classList.remove('inactive');
+        this.redPedestrianLight.classList.remove('inactive');
+        this.greenPedestrianLight.classList.add('inactive');
+    }
+    handleGreenState() {
+        this.redCarLight.classList.add('inactive');
+        this.yellowCarLight.classList.add('inactive');
+        this.greenCarLight.classList.remove('inactive');
+    }
+    handleYellowState() {
+        this.yellowCarLight.classList.remove('inactive');
+        this.greenCarLight.classList.add('inactive');
+    }
+}
+let domManager = new TrafficLightDomManager();
+domManager.work();
--- lab-1/index.ts
+++ lab-1/index.ts
@@ -13,7 +13,8 @@ enum IncomingSignal {
     YELLOW,     // T4
 }
 
-class TrafficLight {
+
+class TrafficLightStateMachine {
     private _state: State;
 
     public get state(): State {
@@ -24,47 +25,164 @@ class TrafficLight {
         this._state = State.OFF;
     }
 
-    public send_signal(signal: IncomingSignal): State {
+    public sendSignal(signal: IncomingSignal): State {
         switch (signal) {
             case IncomingSignal.RED:
-                this.handle_red_signal();
+                this.handleRedSignal();
                 break;
             case IncomingSignal.YELLOW_RED:
-                this.handle_yellow_red_signal();
+                this.handleYellowRedSignal();
                 break;
             case IncomingSignal.GREEN:
-                this.handle_green_signal();
+                this.handleGreenSignal();
                 break;
             case IncomingSignal.YELLOW:
-                this.handle_yellow_signal();
+                this.handleYellowSignal();
                 break;
         }
 
         return this.state;
     }
 
-    private handle_red_signal(): void {
+    private handleRedSignal(): void {
         if (this._state == State.OFF || this._state == State.YELLOW) {
             this._state = State.RED;
         }
     }
 
-    private handle_yellow_red_signal() {
+    private handleYellowRedSignal(): void {
         if (this._state == State.RED) {
             this._state = State.YELLOW_RED;
         }
     }
 
-    private handle_green_signal() {
+    private handleGreenSignal(): void {
         if (this._state == State.YELLOW_RED) {
             this._state = State.GREEN;
         }
     }
 
-    private handle_yellow_signal() {
+    private handleYellowSignal(): void {
         if (this._state == State.GREEN) {
             this._state = State.YELLOW;
         }
     }
 }
 
+
+class TrafficLightDomManager {
+    readonly redCarLight: HTMLElement;
+    readonly yellowCarLight: HTMLElement;
+    readonly greenCarLight: HTMLElement;
+
+    readonly redPedestrianLight: HTMLElement;
+    readonly greenPedestrianLight: HTMLElement;
+
+    private _time: number;
+
+    private stateMachine: TrafficLightStateMachine;
+
+    constructor() {
+        this.redCarLight = document.querySelector('#redCarLight') as HTMLElement;
+        this.yellowCarLight = document.querySelector('#yellowCarLight') as HTMLElement;
+        this.greenCarLight = document.querySelector('#greenCarLight') as HTMLElement;
+        this.redPedestrianLight = document.querySelector('#redPedestrianLight') as HTMLElement;
+        this.greenPedestrianLight = document.querySelector('#greenPedestrianLight') as HTMLElement;
+
+        if (!this.redCarLight || !this.yellowCarLight || !this.greenCarLight ||
+            !this.redPedestrianLight || !this.greenPedestrianLight) {
+            alert('Not all required elements are present on page!');
+        }
+
+        this.stateMachine = new TrafficLightStateMachine();
+        this.handleState();
+
+        this._time = -1;
+    }
+
+    public work(): void {
+        setInterval((): void => {
+            this._time++;
+            this.handleTimer();
+        }, 1000);
+    }
+
+    private handleTimer(): void {
+        if (this._time == 0) {
+            this.sendSignal(IncomingSignal.RED);
+        }
+        else if (this._time == 10) {
+            this.sendSignal(IncomingSignal.YELLOW_RED);
+        }
+        else if (this._time == 12) {
+            this.sendSignal(IncomingSignal.GREEN);
+        }
+        else if (this._time == 22) {
+            this.sendSignal(IncomingSignal.YELLOW);
+        }
+        else if (this._time == 24) {
+            this.sendSignal(IncomingSignal.RED);
+            this._time = -1;
+        }
+    }
+
+    private sendSignal(signal: IncomingSignal): void {
+        this.stateMachine.sendSignal(signal);
+        this.handleState();
+    }
+
+    private handleState(): void {
+        switch (this.stateMachine.state) {
+            case State.OFF:
+                this.handleOffState();
+                break;
+            case State.RED:
+                this.handleRedState();
+                break;
+            case State.YELLOW_RED:
+                this.handleYellowRedState();
+                break;
+            case State.GREEN:
+                this.handleGreenState();
+                break;
+            case State.YELLOW:
+                this.handleYellowState();
+                break;
+        }
+    }
+
+    private handleOffState(): void {
+        this.redCarLight.classList.add('inactive');
+        this.yellowCarLight.classList.add('inactive');
+        this.greenCarLight.classList.add('inactive');
+        this.redPedestrianLight.classList.add('inactive');
+        this.greenPedestrianLight.classList.add('inactive');
+    }
+
+    private handleRedState(): void {
+        this.redCarLight.classList.remove('inactive');
+        this.yellowCarLight.classList.add('inactive');
+        this.redPedestrianLight.classList.add('inactive');
+        this.greenPedestrianLight.classList.remove('inactive');
+    }
+
+    private handleYellowRedState(): void {
+        this.yellowCarLight.classList.remove('inactive');
+        this.redPedestrianLight.classList.remove('inactive');
+        this.greenPedestrianLight.classList.add('inactive');
+    }
+
+    private handleGreenState(): void {
+        this.redCarLight.classList.add('inactive');
+        this.yellowCarLight.classList.add('inactive');
+        this.greenCarLight.classList.remove('inactive');
+    }
+
+    private handleYellowState(): void {
+        this.yellowCarLight.classList.remove('inactive');
+        this.greenCarLight.classList.add('inactive');
+    }
+}
+
+let domManager = new TrafficLightDomManager();
+domManager.work();
